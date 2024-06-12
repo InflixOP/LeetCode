aa6e93ab83058da44cfe8f55c6e369d60a62e1ac
Anubhav Saxena <saxenaanubhav1204@gmail.com>
2024-06-12 13:35:17 +0530

    Create #75 Sort Colors.cpp

---
+++ #75 Sort Colors.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]>nums[j]){
+                    int temp=nums[j];
+                    nums[j]=nums[i];
+                    nums[i]=temp;
+                }
+            }
+        }
+    }
+};
