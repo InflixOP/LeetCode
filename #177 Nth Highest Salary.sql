a86adc1d1dcaf6dee4e801c1e42a153b455ea43d
Anubhav Saxena <saxenaanubhav1204@gmail.com>
2024-06-20 12:21:06 +0530

    Create #177 Nth Highest Salary.sql

---
+++ #177 Nth Highest Salary.sql
@@ -0,0 +1,9 @@
+CREATE FUNCTION getnthhighestsalary(N INT) RETURNS INT
+BEGIN
+  SET N=N-1;
+  RETURN (SELECT DISTINCT salary
+      FROM employee
+      ORDER BY salary DESC
+      LIMIT 1 OFFSET N
+  );
+END
