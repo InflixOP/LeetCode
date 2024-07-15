4dbc9417849f098d38784814761020a56fc8e438
Anubhav Saxena <saxenaanubhav1204@gmail.com>
2024-07-15 20:18:04 +0530

    Create #946 Validate Stack Sequences.cpp

---
+++ #946 Validate Stack Sequences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> st;
+        int j=0;
+        for(int i=0;i<pushed.size();i++){
+            st.push(pushed[i]);
+        while(!st.empty()&&st.top()==popped[j]){
+            st.pop();
+            j++;
+        }
+        }
+        while(!st.empty()&&st.top()==popped[j]&&j<popped.size()){
+            st.pop();
+            j++;
+        }
+        return st.empty();
+    }
+};
