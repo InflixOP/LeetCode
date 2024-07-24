09973053470156887c1b0b5de763ecab11b26d6b
Anubhav Saxena <saxenaanubhav1204@gmail.com>
2024-07-24 12:20:13 +0530

    Update #2191 Sort the Jumbled Numbers.cpp

---
+++ #2191 Sort the Jumbled Numbers.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+    vector<pair<int,int>> ans;
+    for(int i=0;i<nums.size();i++){
+        long long p=nums[i],q=0,r=0;
+        if(p==0){
+            ans.push_back({mapping[0],i});
+            continue;
+        }
+        while(p>0){
+            r+=(mapping[(p%10)]*pow(10,q));
+            q++; 
+            p/=10;
+        }
+        ans.push_back({r,i});
+    }
+    sort(ans.begin(),ans.end());
+    vector<int> a(nums.size());
+    int k=0;
+    for(auto ele:ans){
+        a[k]=nums[ele.second];
+        k++;
+    }   
+    return a;
+    }
+};
