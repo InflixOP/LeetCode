053b40ad9c8c871dc2599dc76f08370e132d53c8
Anubhav Saxena <saxenaanubhav1204@gmail.com>
2024-09-12 11:16:26 +0530

    Update #1684 Count the Number of Consistent Strings.cpp

--- #1684 Count the Number of Consistent Strings.cpp
+++ #1684 Count the Number of Consistent Strings.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int countConsistentStrings(string allowed, vector<string>& words) {
         int c=0;
+        unordered_map<char,int> mp;
+        for(char ele:allowed)
+        mp[ele]++;
         for(int i=0;i<words.size();i++){
             int n=words[i].length();
-            for(char c:words[i]){
-                for(char c1:allowed){
-                    if(c1==c)
-                    n--;
-                }
+            for(char c1:words[i]){
+                if(mp[c1])
+                n--;
             }
             if(n==0)
             c++;
