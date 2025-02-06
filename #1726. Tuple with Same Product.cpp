a2948b9f139c6508c34e96fde50f9534f3ce9e10
Anubhav Saxena <saxenaanubhav1204@gmail.com>
2025-02-06 11:38:17 +0530

    Create #1726. Tuple with Same Product.cpp

---
+++ #1726. Tuple with Same Product.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int tupleSameProduct(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        int ans = 0, n = nums.size();
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++) {
+                int product = nums[i] * nums[j];
+                ans += 8 * mp[product];
+                mp[product]++;
+            }
+        return ans;
+    }
+};
