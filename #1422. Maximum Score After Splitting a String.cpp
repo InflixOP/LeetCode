3c89146684e0e90006f4df208ce47d57da04e8f5
Anubhav Saxena <saxenaanubhav1204@gmail.com>
2025-02-18 15:24:47 +0530

    Update #1422. Maximum Score After Splitting a String.cpp

--- #1422. Maximum Score After Splitting a String.cpp
+++ #1422. Maximum Score After Splitting a String.cpp
@@ -1,30 +1,17 @@
 class Solution {
     public:
         int maxScore(string s) {
-            unordered_map<int,int> mp;
-            unordered_map<int,int> mp1;
-            int c=0;
-            int c1=0;
+            int c=0,c1=0,maxi=0;
             for(int i=0;i<s.length();i++){
                 if(s[i]=='1')
                 c1++;
-                else
-                c++;
             }
-            if(c1==0||c==0)
-            return s.length()-1;
-            c=0;
             for(int i=0;i<s.length()-1;i++){
                 if(s[i]=='0')
                 c++;
                 else
                 c1--;
-                mp[i]=c;
-                mp1[i]=c1;
-            }
-            int maxi=INT_MIN;
-            for(int i=0;i<s.length()-1;i++){
-                maxi=max(maxi,mp[i]+mp1[i]);
+                maxi=max(maxi,c+c1);
             }
             return maxi;
         }
